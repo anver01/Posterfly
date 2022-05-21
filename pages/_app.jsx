796fe6814090c12ab7c6c4fff5210dc5ed339b26
Anshit <anshitverma55@gmail.com>
2022-05-22 02:52:55 +0530

    added nextjs-auth as the authentication service

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { SessionProvider } from 'next-auth/react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import '../styles/global.css'
 
-function MyApp ({ Component, pageProps }) {
+function MyApp ({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Navbar/>
       <Component {...pageProps} />
       <Footer/>
-    </>
+    </SessionProvider>
   )
 }
 
